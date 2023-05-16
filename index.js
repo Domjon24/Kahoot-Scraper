fc9bbc7910782aed1b859284efee99961c856751
Domjon24 <Domjon24@gmail.com>
2023-05-16 00:25:57 -0500

    Testing the screenshot function. 'https://www.youtube.com/watch?v=lgyszZhAZOI&ab_channel=LearnWebCode'

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ const puppeteer = require("puppeteer");
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto("https://quotes.toscrape.com/");
-
     await page.waitForSelector(".col-md-4")
     await page.click(".col-md-4 a")
     await page.waitForSelector("#username")
     await page.type("#username", "Beyonce", {delay: 100});
     await page.type("#password", "1234", {delay: 100});
+    await page.screenshot({path: "TestScreenshot.png"})
     await browser.close();
 })
 
