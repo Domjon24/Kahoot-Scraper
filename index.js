a4867d07df92ba641f72b7328616b04d7ae5b979
Domjon24 <Domjon24@gmail.com>
2023-05-16 00:43:27 -0500

    seperating 'practice' practice from 'kahoot' practice. Delay is fully working now

--- index.js
+++ index.js
@@ -32,19 +32,3 @@
 //     }).catch(err => console.log(err))
 
 //     app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
-
-const puppeteer = require("puppeteer");
-(async (start) => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://quotes.toscrape.com/");
-    await page.waitForSelector(".col-md-4")
-    await page.click(".col-md-4 a")
-    await page.waitForSelector("#username")
-    await page.type("#username", "Beyonce", {delay: 100});
-    await page.type("#password", "1234", {delay: 100});
-    // await browser.close();
-    setTimeout(() => { browser.close(); }, 3000);  
-})
-
-()
