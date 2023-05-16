4daf41a462eb9b6dd658e9f43157acd5b29050ca
Domjon24 <Domjon24@gmail.com>
2023-05-16 00:21:10 -0500

    changing code to test on 'https://quotes.toscrape.com/' website for testing first. commented out old code

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-const PORT = 8000
-const axios = require('axios')
-const cheerio = require('cheerio')
-const express = require('express')
-// const url = 'https://www.stltoday.com/'
-const url = 'https://kahoot.com'
+// const PORT = 8000
+// const axios = require('axios')
+// const cheerio = require('cheerio')
+// const express = require('express')
+// // const url = 'https://www.stltoday.com/'
+// const url = 'https://kahoot.com'
 
 
-const app = express()
+// const app = express()
 
-axios(url)
+// axios(url)
 
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const articles = []
-        // console.log(html)
+//     .then(response => {
+//         const html = response.data
+//         const $ = cheerio.load(html)
+//         const articles = []
+//         // console.log(html)
 
-        $('h1', html).each(function () {
-           const title = $(this).text() 
-           articles.push({
-                title,
-           })
-        })
-        $('.content-container--inner', html).each(function () {
-            const description = $(this).find('p').text()
-            articles.push({
-                 description
-            })
-         })
-        console.log(articles)
-    }).catch(err => console.log(err))
+//         $('h1', html).each(function () {
+//            const title = $(this).text() 
+//            articles.push({
+//                 title,
+//            })
+//         })
+//         $('.content-container--inner', html).each(function () {
+//             const description = $(this).find('p').text()
+//             articles.push({
+//                  description
+//             })
+//          })
+//         console.log(articles)
+//     }).catch(err => console.log(err))
 
-    app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+//     app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
+
+const puppeteer = require("puppeteer");
+(async (start) => {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.goto("https://quotes.toscrape.com/");
+
+    await page.waitForSelector(".col-md-4")
+    await page.click(".col-md-4 a")
+    await page.waitForSelector("#username")
+    await page.type("#username", "Beyonce", {delay: 100});
+    await page.type("#password", "1234", {delay: 100});
+    await browser.close();
+})
+
+()
