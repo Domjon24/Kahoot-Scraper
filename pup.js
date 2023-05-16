7bde854831fae16da5f6d002ec8e63bb576c810f
Domjon24 <Domjon24@gmail.com>
2023-05-16 01:21:12 -0500

    went back and tested with the test website from tutorial vid(https://www.youtube.com/watch?v=lgyszZhAZOI&ab_channel=LearnWebCode). Still need to figure ut why it's not working on original test website (https://quotes.toscrape.com). Something to do with query selector.

--- pup.js
+++ pup.js
@@ -1,15 +1,21 @@
 const puppeteer = require("puppeteer");
+const fs = require('fs/promises');
 (async (start) => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("https://quotes.toscrape.com/");
-    await page.waitForSelector(".col-md-4")
-    await page.click(".col-md-4 a")
-    await page.waitForSelector("#username")
-    await page.type("#username", "Beyonce", {delay: 100});
-    await page.type("#password", "1234", {delay: 100});
-    // await browser.close();
-    setTimeout(() => { browser.close(); }, 3000);  
+    // const names = ["123", "456", "789"]
+    await page.goto("https://learnwebcode.github.io/practice-requests");          /*Original website https://quotes.toscrape.com/*/
+    // await page.waitForSelector(".col-md-4")
+    // await page.click(".col-md-4 a")
+    // await page.waitForSelector("#username")
+    // await page.type("#username", "Beyonce");                  /*, {delay: 100}  <-- add this after beyonce for delay*/ 
+    // await page.type("#password", "1234"/*, {delay: 100}*/);
+    // // class(span) = text. id = text
+    const names = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
+    })
+    await fs.writeFile("names.txt", names.join("\r\n"));
+    setTimeout(() => { browser.close(); }, 100);  
 })
 
 ()
